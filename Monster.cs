d7835b4499a36012cb5ee70df92c2937f5472f7e
thanya0092-bit <thanya0092@gmail.com>
2026-03-01 18:46:39 +0100

    Add files via upload

---
+++ Monster.cs
@@ -0,0 +1,20 @@
+﻿
+public class Monster
+{
+    public int ID;
+    public string Name;
+    public int MaximumDamage;
+    public int RewardExperiencePoints { get; set; }
+    public int MaximumHitPoints { get; set; }
+    public int CurrentHitPoints { get; set; }
+
+    public Monster(int id, string name, int maximumDamage, int rewardExperiencePoints, int maximumHitPoints)
+    {
+        ID = id;
+        Name = name;
+        MaximumDamage = maximumDamage;
+        RewardExperiencePoints = rewardExperiencePoints;
+        MaximumHitPoints = maximumHitPoints;
+        CurrentHitPoints = maximumHitPoints;
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,15 @@
+﻿public class Player
+{
+    public int CurrentHitPoints { get; set; }
+    public int MaximumHitPoints { get; set; }
+    public int ExperiencePoints { get; set; }
+    public Weapon CurrentWeapon { get; set; }
+
+    public Player(int maximumHitPoints, Weapon startingWeapon)
+    {
+        MaximumHitPoints = maximumHitPoints;
+        CurrentHitPoints = maximumHitPoints;
+        ExperiencePoints = 0;
+        CurrentWeapon = startingWeapon;
+    }
+}
