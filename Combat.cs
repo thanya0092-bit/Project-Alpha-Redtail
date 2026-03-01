dafb62a706fd082ef3c9f79ce273abfd89903b57
thanya0092-bit <thanya0092@gmail.com>
2026-03-01 18:51:31 +0100

    Update Combat.cs

--- Combat.cs
+++ Combat.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public static class Combat
+{
+    public static void AttackMonster(Player player, Monster monster)
+    {
+        int damageToMonster = World.RandomGenerator.Next(1, player.CurrentWeapon.MaximumDamage + 1);
+        monster.CurrentHitPoints -= damageToMonster;
+
+        Console.WriteLine($"You hit the {monster.Name} for {damageToMonster} damage.");
+
+        if (monster.CurrentHitPoints <= 0)
+        {
+            Console.WriteLine($"You defeated the {monster.Name}!");
+            player.ExperiencePoints += monster.RewardExperiencePoints;
+            Console.WriteLine($"You gain {monster.RewardExperiencePoints} XP.");
+        }
+        else
+        {
+            AttackPlayer(player, monster);
+        }
+    }
+
+    private static void AttackPlayer(Player player, Monster monster)
+    {
+        int damageToPlayer = World.RandomGenerator.Next(1, monster.MaximumDamage + 1);
+        player.CurrentHitPoints -= damageToPlayer;
+
+        Console.WriteLine($"The {monster.Name} hit you for {damageToPlayer} damage.");
+
+        if (player.CurrentHitPoints <= 0)
+        {
+            Console.WriteLine("You died...");
+        }
+    }
+}
