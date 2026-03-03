3cb5964ca38cf0e7f75bab222f5f3728070de282
am0392 <andymorgan@hotmail.nl>
2026-03-03 10:37:09 +0100

    Update Program.cs
    Working loop for moving around

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 {
     public static void Main()
     {
-        // Create player
         Player player = new Player(
             20,
             World.WeaponByID(World.WEAPON_ID_RUSTY_SWORD)
@@ -11,14 +10,17 @@
         // Get a monster from World
         Monster Rat1 = World.MonsterByID(World.MONSTER_ID_RAT);
 
-        // Fight until someone dies
-        while (player.CurrentHitPoints > 0 && Rat1.CurrentHitPoints > 0)
+        bool running = true;
+        while (running)
         {
             Console.WriteLine($"You are at: {World.CurrentLocation.Name}. From here you can go:");
             Console.WriteLine(World.CurrentLocation.Compass());
             string input = Console.ReadLine()!;
             World.TryToMoveTo(World.CurrentLocation.GetLocationAt(input));
-            break;
+            if (World.CurrentLocation == World.LocationByID(9))
+            {
+                break;   
+            }
         }
     }
 }
