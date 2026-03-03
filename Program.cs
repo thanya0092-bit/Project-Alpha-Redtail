60213b1faa38ea9b198f071eff86ed678f203d94
am0392 <andymorgan@hotmail.nl>
2026-03-03 22:37:30 +0100

    Start of game loop

--- Program.cs
+++ Program.cs
@@ -5,16 +5,37 @@
         bool running = true;
         while (running)
         {
-            Console.WriteLine($"You are at: {World.CurrentLocation.Name}. From here you can go:");
-            Console.WriteLine(World.CurrentLocation.Compass());
-            string input = Console.ReadLine()!;
-            if (!World.TryToMoveTo(World.CurrentLocation.GetLocationAt(input)))
+            Console.WriteLine($"You are at: {World.CurrentLocation.Name}.");
+            Console.WriteLine("What would you like to do? (enter a number)");
+            Console.WriteLine("1. Move");
+            Console.WriteLine("2. Fight");
+            Console.WriteLine("3. Quit");
+            string choice = Console.ReadLine()!;
+            switch (choice)
             {
-                Console.WriteLine("That is not a valid direction!");
-            }
-            if (World.CurrentLocation == World.LocationByID(9))
-            {
-                break;   
+                case "1":
+                    // Print map
+                    Console.WriteLine("\n");
+                    Console.WriteLine($"You are at: {World.CurrentLocation.Name}. From here you can go:");
+                    Console.WriteLine(World.CurrentLocation.Compass());
+                    string userinput = Console.ReadLine()!;
+                    if (!World.TryToMoveTo(World.CurrentLocation.GetLocationAt(userinput)))
+                    {
+                        Console.WriteLine("That is not a valid direction!");
+                    }
+
+                    // if World.CurrentLocation.QuestAvailableHere
+
+                    break;
+                case "2":
+                    // ...
+                    break;
+                case "3":
+                    return;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+
             }
         }
     }
