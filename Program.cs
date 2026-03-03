b8e53080af9cc3e460f57f4ff0a9d7fb88037ecb
am0392 <andymorgan@hotmail.nl>
2026-03-03 13:04:50 +0100

    Invalid direction message

--- Program.cs
+++ Program.cs
@@ -2,21 +2,16 @@
 {
     public static void Main()
     {
-        Player player = new Player(
-            20,
-            World.WeaponByID(World.WEAPON_ID_RUSTY_SWORD)
-        );
-
-        // Get a monster from World
-        Monster Rat1 = World.MonsterByID(World.MONSTER_ID_RAT);
-
         bool running = true;
         while (running)
         {
             Console.WriteLine($"You are at: {World.CurrentLocation.Name}. From here you can go:");
             Console.WriteLine(World.CurrentLocation.Compass());
             string input = Console.ReadLine()!;
-            World.TryToMoveTo(World.CurrentLocation.GetLocationAt(input));
+            if (!World.TryToMoveTo(World.CurrentLocation.GetLocationAt(input)))
+            {
+                Console.WriteLine("That is not a valid direction!");
+            }
             if (World.CurrentLocation == World.LocationByID(9))
             {
                 break;   
